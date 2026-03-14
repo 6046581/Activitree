1b102092b3d0a6f9e80823c679c724501a6f7c33
NoomStuff <NoomStuffMail@gmail.com>
2026-03-14 04:58:39 +0100

    Popup work

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,15 @@
 export function isLoggedIn(): boolean {
-  return !!localStorage.getItem("authToken");
+   return !!localStorage.getItem("authToken");
 }
 
 export function login(token: string) {
-  localStorage.setItem("authToken", token);
+   localStorage.setItem("authToken", token);
 }
 
 export function logout() {
-  localStorage.removeItem("authToken");
+   localStorage.removeItem("authToken");
 }
 
 export function getToken(): string | null {
-  return localStorage.getItem("authToken");
+   return localStorage.getItem("authToken");
 }
