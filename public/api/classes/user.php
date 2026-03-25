d2f7eb2c3da8f9765bb8224cc5c26663b030a474
Billy <62342303+JBGhostly@users.noreply.github.com>
2026-03-25 23:45:08 +0100

    user class

---
+++ public/api/classes/user.php
@@ -0,0 +1,76 @@
+<?php
+class Users
+{
+    private $conn;
+    private $table = "users";
+
+    public $id;
+    public $username;
+    public $email;
+    public $password;
+    public $role;
+
+    public function __construct($database = null)
+    {
+        if ($database === null) {
+            $database = new Database();
+        }
+
+        $this->conn = $database->connect();
+    }
+
+    public function getById($id)
+    {
+        $query = "SELECT id, username, email, role FROM" . $this->table . "WHERE id = :id LIMIT 1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function createUser($username, $email, $password, $role = "user")
+    {
+        $query = "INSERT INTO" . $this->table . "(username, email, password, role) VALUES (:username, :email, :password, :role)";
+        $stmt = $this->conn->prepare($query);
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam("email:", $email);
+        $stmt->bindParam(":password", $password);
+        $stmt->bindParam(":role", $role);
+        return $stmt->execute;
+    }
+
+    public function updateUser($id, $username, $email, $role)
+    {
+        $query = "UPDATE " . $this->table . "SET username = :username, email = :email, role = :role WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id, $id, PDO::PARAM_INT");
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":role", $role);
+        return $stmt->execute;
+    }
+
+    public function login($email, $password)
+    {
+        $query = "SELECT * FROM" . $this->table . "WHERE email = :email LIMIT 1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            unset($user['password']);
+            return $user;
+        }
+        return false;
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM" . $this->table . "WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+}
