084108259f5ad6507145f6a34be3d584376c76f7
NoomStuff <NoomStuffMail@gmail.com>
2026-06-22 02:01:45 +0200

    Update deps to latest

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+import * as rippleCompiler from "ripple/compiler";
 import ripple from "@ripple-ts/eslint-plugin";
+
+globalThis.__RIPPLE_COMPILER__ = rippleCompiler;
 
 export default [...ripple.configs.recommended];
